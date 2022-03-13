e76686a29ec3fb184f540d7b79ca5c0b3b912640
DuongXuanHuy-207PM68593 <alexandra89610@gmail.com>
2022-03-13 17:37:10 +0700

    khoi tao files

---
+++ SUItutorial/SUItutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUItutorialApp.swift
+//  SUItutorial
+//
+//  Created by Duong Huy on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SUItutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
